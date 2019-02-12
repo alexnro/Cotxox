5bb1e4bb8fc0ad3db24833792f7d37ce78883262
alexnro <alexjandron99@gmail.com>
2019-02-12 12:31:11 +0100

    Created variables and getters

--- src/main/java/org/lasencinas/tarifa/Tarifa.java
+++ src/main/java/org/lasencinas/tarifa/Tarifa.java
@@ -1,4 +1,41 @@
 package org.lasencinas.tarifa;
 
 public class Tarifa {
+
+    /*-------------- Variables -------------*/
+
+    private double costeMilla = 1.35;
+    private double costeMinuto = 0.35;
+    private int costeMinimo = 5;
+    private int porcentajeComision = 20;
+    private double costeTotalEsperado = 0;
+
+    /*-------------- Builders ----------------*/
+
+    public Tarifa() {}
+
+    public Tarifa(double costeMilla, double costeMinuto, int costeMinimo, int porcentajeComision) {
+        this.costeMilla = costeMilla;
+        this.costeMinuto = costeMinuto;
+        this.costeMinimo = costeMinimo;
+        this.porcentajeComision = porcentajeComision;
+        this.costeTotalEsperado = costeTotalEsperado;
+    }
+
+    /*---------------- Getters ------------------*/
+
+    public double getCosteDistancia(double distancia) {
+        setCosteDistancia();
+        return this.costeMilla;
+    }
+
+    public double getCosteTiempo(int minutos) {
+        setCosteTiempo();
+        return this.costeMinuto;
+    }
+
+    public double getCosteTotalEsperado() {
+        setCosteTotalEseprado();
+        return this.costeTotalEsperado;
+    }
 }
