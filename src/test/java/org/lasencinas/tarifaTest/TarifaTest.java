c4d37e30a2a665a4148b83f1b8170525c7638bd9
alexnro <alexjandron99@gmail.com>
2019-02-12 12:56:54 +0100

    Added test cases for getCosteDistanciaTest()

--- src/test/java/org/lasencinas/tarifaTest/TarifaTest.java
+++ src/test/java/org/lasencinas/tarifaTest/TarifaTest.java
@@ -3,20 +3,32 @@ package org.lasencinas.tarifaTest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.lasencinas.tarifa.Tarifa;
 
 
 public class TarifaTest {
 
+    private Tarifa tarifa = null;
+
+    @Before
+    public void init() {
+
+        tarifa = new Tarifa();
+    }
     @Test
     public void gettersTest() {
-
-        Tarifa tarifa = new Tarifa();
 
         assertEquals(1.35, tarifa.getCosteMilla(), 0);
         assertEquals(0.35, tarifa.getCosteMinuto(), 0);
         assertEquals(5, tarifa.getCosteMinimo());
         assertEquals(20, tarifa.getPorcentajeComision());
+    }
+
+    @Test
+    public void getCosteDistanciaTest() {
+
+        assertEquals(10.46, tarifa.getCosteDistancia(7.75), 0.005);
     }
 }
