981a4f9f5770ccad712bf1fc5d4c370d90f2f4b3
alexnro <alexjandron99@gmail.com>
2019-02-17 04:42:17 +0100

    .

--- src/main/java/org/lasencinas/conductores/Conductor.java
+++ src/main/java/org/lasencinas/conductores/Conductor.java
@@ -1,4 +1,53 @@
 package org.lasencinas.conductores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conductor {
+
+    private String nombre = null;
+    private String modeloCoche = null;
+    private String matricula = null;
+    private int valoracionMedia = 0;
+    private List<Integer> valoraciones = new ArrayList<Integer>();
+    private boolean ocupado = false;
+
+    public Conductor() {}
+
+    public Conductor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setModeloCoche(String modeloCoche) {
+        this.modeloCoche = modeloCoche;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setValoracion(int valoracion) {
+        valoraciones.add(valoracion);
+        int auxiliar = 0;
+        for (int valor : valoraciones) {
+            auxiliar += valor;
+        }
+        valoracionMedia = auxiliar / valoraciones.size()-1;
+    }
+
+    public String getModeloCoche() {
+        return modeloCoche;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public int getValoracionMedia() {
+        return valoracionMedia;
+    }
+
+    public List<Integer> getValoraciones() {
+        return valoraciones;
+    }
 }
---
+++ src/test/java/org/lasencinas/conductoresTest/ConductorTest.java
@@ -0,0 +1,4 @@
+package org.lasencinas.conductoresTest;
+
+public class ConductorTest {
+}
